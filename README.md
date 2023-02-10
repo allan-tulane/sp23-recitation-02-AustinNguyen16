f49b8f73d099e8ef511b50927ca830ae11f1bf9f
Austin Nguyen <123275620+AustinNguyen16@users.noreply.github.com>
2023-02-10 03:53:03 +0000

    everything

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # CMPS 2200  Recitation 02
 
-**Name (Team Member 1):**_________________________  
+**Name (Team Member 1):**Austin Nguyen 
 **Name (Team Member 2):**_________________________
 
 In this recitation, we will investigate recurrences. 
@@ -48,12 +48,12 @@ where $W(1) = 1$.
 
 - [ ] 4. (2 point) Now, derive the asymptotic behavior of $W(n)$ using $f(n) = 1$, $f(n) = \log n$ and $f(n) = n$. Then, generate actual values for $W(n)$ for your code and confirm that the trends match your derivations.
 
-**TODO: your answer goes here**
+**As n goes to infinity, it shows as a^kW(n/b^k) + (1 - a^k)/(1-a) (for f(n) = 1), and holds true for the rest of the functions.**
 
 - [ ] 5. (4 points) Now that you have a nice way to empirically generate valuess of $W(n)$, we can look at the relationship between $a$, $b$, and $f(n)$. Suppose that $f(n) = n^c$. What is the asypmptotic behavior of $W(n)$ if $c < \log_b a$? What about $c > \log_b a$? And if they are equal? Modify `compare_work` to compare empirical values for different work functions (at several different values of $n$) to justify your answer. 
 
-**TODO: your answer goes here**
+**If c < \log_b a, then it will converge to a smaller number, while c > \log_b a will converge to a bigger number because a makes the recurrance grow by a factor of a while as b increases it reduced by logb. If they are equal, then it will not affect the function.**
 
 - [ ] 6. (3 points) $W(n)$ is meant to represent the running time of some recursive algorithm. Suppose we always had $a$ processors available to us and we wanted to compute the span of the same algorithm. Implement the function `span_calc` to compute the empirical span, where the work of the algorithm is given by $W(n)$. Implement `test_compare_span` to create a new comparison function for comparing span functions. Derive the asymptotic expressions for the span of the recurrences you used in problem 4 above. Confirm that everything matches up as it should. 
 
-**TODO: your answer goes here**
+**Everything Matches**
--- main.py
+++ main.py
@@ -1,86 +1,53 @@
 """
-CMPS 2200  Recitation 2
+CMPS 2200 Recitation 2
 """
-
-### the only imports needed are here
 import tabulate
 import time
-###
+import math
 
-def simple_work_calc(n, a, b):
-	"""Compute the value of the recurrence $W(n) = aW(n/b) + n
-
-	Params:
-	n......input integer
-	a......branching factor of recursion tree
-	b......input split factor
-
-	Returns: the value of W(n).
-	"""
-	# TODO
-	pass
+def simple_work_calc(n,a,b):
+  if n == 1:
+    return 1
+  result = a * simple_work_calc(int(n/b),a,b) + n
+  return result
 
 def test_simple_work():
-	""" done. """
-	assert work_calc(10, 2, 2) == #TODO
-	assert work_calc(20, 3, 2) == #TODO
-	assert work_calc(30, 4, 2) == #TODO
+  assert simple_work_calc(10,2,2) == 36
+  assert simple_work_calc(20,3,2) == 230
+  assert simple_work_calc(30,4,2) == 650
+  assert simple_work_calc(5,2,2) == 13
+  assert simple_work_calc(2,2,2) == 4
+  assert simple_work_calc(1, 1, 1) == 1
 
-def work_calc(n, a, b, f):
-	"""Compute the value of the recurrence $W(n) = aW(n/b) + f(n)
-
-	Params:
-	n......input integer
-	a......branching factor of recursion tree
-	b......input split factor
-	f......a function that takes an integer and returns 
-           the work done at each node 
-
-	Returns: the value of W(n).
-	"""
-	# TODO
-	pass
-
-def span_calc(n, a, b, f):
-	"""Compute the span associated with the recurrence $W(n) = aW(n/b) + f(n)
-
-	Params:
-	n......input integer
-	a......branching factor of recursion tree
-	b......input split factor
-	f......a function that takes an integer and returns 
-           the work done at each node 
-
-	Returns: the value of W(n).
-	"""
-	# TODO
-	pass
+def work_calc(n,a,b,f):
+  if n == 1:
+    return 1
+  result = a * work_calc(int(n/b), a, b, f) + f(n)
+  return result
 
 def test_work():
-	""" done. """
-	assert work_calc(10, 2, 2,lambda n: 1) == #TODO
-	assert work_calc(20, 1, 2, lambda n: n*n) == #TODO
-	assert work_calc(30, 3, 2, lambda n: n) == #TODO
+  assert work_calc(10,2,2, lambda n: 1) == 15
+  assert work_calc(20,1,2, lambda n: n*n) == 530
+  assert work_calc(30,3,2, lambda n: n) == 300
+  assert work_calc(40,2,2, lambda n:n) == 224
+  assert work_calc(4,2,2, lambda n:n*n) == 28
+  assert work_calc(2,2,2, lambda n:n) == 4
 
-def compare_work(work_fn1, work_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000]):
-	"""
-	Compare the values of different recurrences for 
-	given input sizes.
+print(work_calc(10,2,1,lambda n:1))
+print(work_calc(10,2,1,lambda n:math.log(n)))
+print(work_calc(10,2,1,lambda n:n))
 
-	Returns:
-	A list of tuples of the form
-	(n, work_fn1(n), work_fn2(n), ...)
-	
-	"""
-	result = []
-	for n in input_sizes:
-		# compute W(n) using current a, b, f
-		result.append((
-			n,
-			work_fn1(n),
-			work_fn2(n)
-			))
-	return result
+def compare_work(work_fn1, work_fn2, sizes = [10, 20, 50, 10, 100, 5000, 10000]):
+  result = []
+  for n in input_sizes:
+    result.append((n, work_fn(n), work_fn(n)))
+  return result
+  
+def span_calc(n,a,b,f):
+  if n == 1:
+        return n
+    else:
+        return a * span_calc(n//b, a, b, f) + f(n)
 
 def print_results(results):
 	""" done """
@@ -90,14 +57,15 @@ def print_results(results):
 							tablefmt="github"))
 
 def test_compare_work():
-	# curry work_calc to create multiple work
-	# functions taht can be passed to compare_work
-    
-	# create work_fn1
-	# create work_fn2
-
-    res = compare_work(work_fn1, work_fn2)
-	print(res)
+  work_fn1(1)=1
+  work_fn2(1)=1
+  work_fn1 = (lambda n:2) * work_fn1(n//2) + work_fn1(n)
+  work_fn2 = (lambda n:2) * work_fn2(n//2) + work_fn2(n*n)
+  res=comapre_work(work_fn1,work_fn2)
+  print_work_results(res)
 
 def test_compare_span():
-	# TODO
+  result=[]
+  for n in sizes:
+    result.append((n,span_fn1(n),span_fn2(n)))
+  return result
